846f66d742f9eb91507304421a7331f44bd73d77
Greg <47268138+gregsypek@users.noreply.github.com>
2024-12-10 09:17:36 +0100

    Eslint & Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 const Home = () => {
-	return (
-		<h1 className="text-3xl text-white font-black">
-			Welcome to the world of Next.js
-		</h1>
-	);
+  return (
+    <h1 className="text-3xl font-black text-white">
+      Welcome to the world of Next.js
+    </h1>
+  );
 };
 
 export default Home;
