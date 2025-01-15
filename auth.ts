bbae87b3672337278f48aa1463c9c6a9fdf52410
Greg <47268138+gregsypek@users.noreply.github.com>
2025-01-15 17:30:15 +0100

    Implement Password/Email Auth

--- auth.ts
+++ auth.ts
@@ -1,9 +1,13 @@
+import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 import { IAccountDoc } from "./database/account.model";
+import { IUserDoc } from "./database/user.model";
 import { api } from "./lib/api";
+import { SignInSchema } from "./lib/validations";
 
 // We'll check if the sign-in account type is credetntials; if yes, then we skip. We'll handle it the other way around when doing email password-based authentication.
 
@@ -11,7 +15,47 @@ import { api } from "./lib/api";
 
 // NextAuth: Biblioteka do zarządzania uwierzytelnianiem w aplikacjach Next.js.
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub, Google],
+  providers: [
+    GitHub,
+    Google,
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = SignInSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const { data: existingAccount } = (await api.accounts.getByProvider(
+            email
+          )) as ActionResponse<IAccountDoc>;
+
+          if (!existingAccount) return null;
+
+          const { data: existingUser } = (await api.users.getById(
+            existingAccount.userId.toString()
+          )) as ActionResponse<IUserDoc>;
+
+          if (!existingUser) return null;
+
+          const isValidPassword = await bcrypt.compare(
+            password,
+            existingAccount.password!
+          );
+
+          if (isValidPassword) {
+            return {
+              id: existingUser.id,
+              name: existingUser.name,
+              email: existingUser.email,
+              image: existingUser.image,
+            };
+          }
+        }
+
+        return null;
+      },
+    }),
+  ],
   callbacks: {
     async signIn({ user, profile, account }) {
       if (account?.type === "credentials") return true;
