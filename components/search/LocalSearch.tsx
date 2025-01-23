8d661d47fa889ce6d1c7bb5b13ce5338f7425fc8
Greg <47268138+gregsypek@users.noreply.github.com>
2025-01-23 09:50:42 +0100

    Display Tags

--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -13,9 +13,16 @@ interface Props {
   imgSrc: string;
   placeholder: string;
   otherClasses?: string;
+  iconPosition?: "left" | "right";
 }
 
-const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
+const LocalSearch = ({
+  route,
+  imgSrc,
+  placeholder,
+  otherClasses,
+  iconPosition = "left",
+}: Props) => {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
@@ -53,13 +60,15 @@ const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
     >
       {/* {searchParams.toString()} */}
       <label htmlFor="search">
-        <Image
-          src={imgSrc}
-          width={24}
-          height={24}
-          alt="Search"
-          className="cursor-pointer"
-        />
+        {iconPosition === "left" && (
+          <Image
+            src={imgSrc}
+            width={24}
+            height={24}
+            alt="Search"
+            className="cursor-pointer"
+          />
+        )}
       </label>
       <Input
         type="text"
@@ -69,6 +78,16 @@ const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
         onChange={(e) => setSearchQuery(e.target.value)}
         className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
       />
+
+      {iconPosition === "right" && (
+        <Image
+          src={imgSrc}
+          width={15}
+          height={15}
+          alt="Search"
+          className="cursor-pointer"
+        />
+      )}
     </div>
   );
 };
